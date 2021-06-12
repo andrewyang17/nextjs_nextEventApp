0408b43a6cb43b7a114aed2c95e1a1f9a9edb657
andrewyang17 <50484992+andrewyang17@users.noreply.github.com>
2021-06-11 23:43:12 -0700

    wrap with Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
 import Head from "next/head";
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import {NotificationContextProvider} from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <NotificationContextProvider>
+      <Layout>
         <Head>
           <meta name="description" content="NextEvents" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-      <Component {...pageProps} />
-    </Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
