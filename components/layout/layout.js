33667fcbb125e18a75e7e4a291e4119c8ae8ca51
andrewyang17 <50484992+andrewyang17@users.noreply.github.com>
2021-06-11 23:41:57 -0700

    layout component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,20 @@
-import { Fragment } from 'react';
+import {Fragment, useContext} from 'react';
 
 import MainHeader from './main-header';
+import Notification from "../ui/notification";
+import {NotificationContext} from "../../store/notification-context";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification
   return (
     <Fragment>
-      <MainHeader />
+      <MainHeader/>
       <main>{props.children}</main>
+      {activeNotification &&
+      (<Notification title={activeNotification.title} message={activeNotification.message}
+                     status={activeNotification.status}/>)
+      }
     </Fragment>
   );
 }
